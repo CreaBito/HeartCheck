be2f5da8fdeefa558ae9f997d29d5a3efa3cb629
CreaBito <creabit7@gmail.com>
2026-04-25 18:52:28 +0500

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Елисей')
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit
+from instr.py import *
+
+class MainWin(QWidget):
+  def __init__(self) 
+    super().__init__()
+    def set_appear(self):
+      self.setWindowTitle(txt_title)
+      self.resize(win_w,win_h)
+    def initUI(self):
+      pass
+    def
+
+
