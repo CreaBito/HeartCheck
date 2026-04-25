db79d9691ddbe042fd4db4b94bc19723cdbf7931
CreaBito <creabit7@gmail.com>
2026-04-25 20:49:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit
 from instr import *
+from second_win import *
 
 class MainWin(QWidget):
     def __init__(self) :
@@ -13,18 +14,18 @@ class MainWin(QWidget):
   
         self.show()
     def initUI(self):
-      self.text_hi = QLabel(text_hi)
+      self.text_hi = QLabel(txt_hi)
       self.text_instruction = QLabel(txt_instruction)
       self.btn_next = QPushButton(tx_next, self)
 
       self.layout_line = QVBoxLayout()
 
       self.layout_line.addWidget(self.text_hi, alignment = Qt.AlignLeft)
-      self.layout_line.addWidget(self.txt_instruction, alignment = Qt.AlignLeft)
+      self.layout_line.addWidget(self.text_instruction, alignment = Qt.AlignLeft)
       self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
       self.setLayout(self.layout_line)
     
-    def cllick_pushnext(self):
+    def click_pushnext(self):
         self.tw = TestWin()
         self.hide()
     def connects(self):
@@ -35,13 +36,8 @@ class MainWin(QWidget):
       self.setWindowTitle(txt_title)
       self.resize(win_w,win_h)
       
-    def initUI(self):
-      pass
-    def
 
 
 app = QApplication([])
 mw = MainWin()
 app.exec_()
-
-
